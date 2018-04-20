c2b5c39128a28e0daa904ac26af0f511306d7a07
Peter Wanden <peter.wanden@gmail.com>
2018-04-20 13:27:08 +0100

    complete todo's

--- app/src/main/java/com/example/android/mygarden/PlantWateringService.java
+++ app/src/main/java/com/example/android/mygarden/PlantWateringService.java
@@ -17,13 +17,16 @@ package com.example.android.mygarden;
 */
 
 import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.provider.Settings;
 
-import com.example.android.mygarden.provider.PlantContract;
+import com.example.android.mygarden.provider.PlantContract.PlantEntry;
 import com.example.android.mygarden.utils.PlantUtils;
 
 import static com.example.android.mygarden.provider.PlantContract.BASE_CONTENT_URI;
@@ -36,9 +39,8 @@ import static com.example.android.mygarden.provider.PlantContract.PATH_PLANTS;
 public class PlantWateringService extends IntentService {
 
     public static final String ACTION_WATER_PLANTS = "com.example.android.mygarden.action.water_plants";
-    // TODO (3): Create a new action ACTION_UPDATE_PLANT_WIDGETS to handle updating widget UI and
-    // implement handleActionUpdatePlantWidgets to query the plant closest to dying and call
-    // updatePlantWidgets to refresh widgets
+    // TODO (3): Create a new action ACTION_UPDATE_PLANT_WIDGETS...
+    public static final String ACTION_UPDATE_PLANT_WIDGETS = "com.example.android.mygarden.action.update_plant_widgets";
 
 
     public PlantWateringService() {
@@ -57,6 +59,13 @@ public class PlantWateringService extends IntentService {
         context.startService(intent);
     }
 
+    // Todo (3.1) ... to handle updating widget UI
+    public static void startActionUpdatePlantWidgets(Context context) {
+        Intent intent = new Intent(context, PlantWateringService.class);
+        intent.setAction(ACTION_UPDATE_PLANT_WIDGETS);
+        context.startService(intent);
+    }
+
     /**
      * @param intent
      */
@@ -65,7 +74,7 @@ public class PlantWateringService extends IntentService {
         if (intent != null) {
             final String action = intent.getAction();
             if (ACTION_WATER_PLANTS.equals(action)) {
-                handleActionWaterPlants();
+                handleActionUpdatePlantWidgets();
             }
         }
     }
@@ -78,12 +87,58 @@ public class PlantWateringService extends IntentService {
         Uri PLANTS_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
         ContentValues contentValues = new ContentValues();
         long timeNow = System.currentTimeMillis();
-        contentValues.put(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME, timeNow);
+        contentValues.put(PlantEntry.COLUMN_LAST_WATERED_TIME, timeNow);
         // Update only plants that are still alive
         getContentResolver().update(
                 PLANTS_URI,
                 contentValues,
-                PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME+">?",
+                PlantEntry.COLUMN_LAST_WATERED_TIME+">?",
                 new String[]{String.valueOf(timeNow - PlantUtils.MAX_AGE_WITHOUT_WATER)});
+    }
+
+    // Todo (3.5) Implement handleActionUpdatePlantWidgets to query the plant closest to dying
+    private void handleActionUpdatePlantWidgets() {
+
+        // Query the DB to find the plant that needs water the most
+        Uri PLANT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
+        Cursor cursor = getContentResolver().query(
+                PLANT_URI,
+                null,
+                null,
+                null,
+                PlantEntry.COLUMN_LAST_WATERED_TIME
+        );
+
+        // Default image, in case no plants in garden
+        int imgRes = R.drawable.grass;
+
+        // Extract the plant details
+        if (cursor != null && cursor.getCount() >0) {
+            cursor.moveToFirst();
+
+            // Get the current time.
+            long timeNow = System.currentTimeMillis();
+
+            // Get the plant values
+            long wateredAt = cursor.getLong(cursor.getColumnIndex(PlantEntry.COLUMN_CREATION_TIME));
+            long createdAt = cursor.getLong(cursor.getColumnIndex(PlantEntry.COLUMN_LAST_WATERED_TIME));
+            int plantType = cursor.getInt(cursor.getColumnIndex(PlantEntry.COLUMN_PLANT_TYPE));
+
+            // Close the cursor
+            cursor.close();
+
+            imgRes = PlantUtils.getPlantImageRes(
+                    this,
+                    timeNow - createdAt,
+                    timeNow - wateredAt,
+                    plantType);
+        }
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(
+                this, PlantWidgetProvider.class));
+
+        // Todo (3.6) call updatePlantWidgets to refresh widgets
+        PlantWidgetProvider.updatePlantWidgets(this, appWidgetManager, imgRes, appWidgetIds);
     }
 }
